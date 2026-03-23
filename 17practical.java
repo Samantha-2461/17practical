c8963279bd839624fc226609df9d62ad9732a1a2
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-23 14:57:53 +0200

    Update 17practical.java

--- 17practical.java
+++ 17practical.java
@@ -1,5 +1,58 @@
 /*I acknowledge the use of a large language model called Deepseek to assist me to understand what is being 
 asked in the practical, debug my code and understand more about binary search trees
   */
-import java.io.*;
 import java.util.*;
+
+class tNode{
+ int key;
+ tNode left;
+ tNode right;
+
+ tNode(int key){
+  this.key=key;
+  this.left=null;
+  this.right=null;
+ }
+ public class tryBST {
+  private tNode root;
+
+  public tryBST(){
+   this.root=null; 
+  }
+  public void insert(int key) {
+   root = insertRec (root, key);
+  }
+  private tNode insertRec(tNode node, int key){
+   if (node ==null){
+    return new tNode(key);
+   }
+   if (key <node.key) {
+    node.left = insertRec(node.left, key);
+   } else if (key > node.key){
+    node.right = insertRecZ(node.right, key);
+   }
+   return node;
+   } 
+  public boolean isBST(){
+   return isBSTRec(root, Interger.MIN_VALUE, Interger.MAX_VALUE);
+   }
+  private boolean isBSTRec(tNode node, int min, int max){
+   if (node ==null) {
+    return true;
+   }
+   if (node.key <min|| node.key>max){
+    return false;
+   }
+   return isBSTRec(node.left, min, node.key-1)&&
+    isBSTRec(node.right, node.key +1, max);
+  }
+  public void buildBalancedBST(int n){
+   int totalKeys = (int)Math.pow(2,n)-1;
+   root =null;
+   buildBalancedRec(1, totalKeys);
+  }
+  
+  }
+  }
+ }
+}
