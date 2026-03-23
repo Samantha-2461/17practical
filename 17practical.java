479df9eaeaa3cfdbabf86ad802a256fdf56cc1f8
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-23 15:38:20 +0200

    Update 17practical.java

--- 17practical.java
+++ 17practical.java
@@ -1,10 +1,7 @@
 /*I acknowledge the use of a large language model called Deepseek to assist me to understand what is being 
 asked in the practical, debug my code and understand more about binary search trees
   */
-/**
- * Practical 7 - Binary Search Tree
- * Simplified version with all code in one class
- */
+
 
 import java.util.*;
 
