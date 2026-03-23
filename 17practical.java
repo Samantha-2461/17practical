c259dbabf99bfde7721c937778cd763f7387571f
Samantha-2461 <siwelasamantha49@gmail.com>
2026-03-23 14:27:25 +0200

    Create 17practical.java

---
+++ 17practical.java
@@ -0,0 +1,5 @@
+/*I acknowledge the use of a large language model called Deepseek to assist me to understand what is being 
+asked in the practical, debug my code and understand more about binary search trees
+  */
+import java.io.*;
+import java.util.*;
